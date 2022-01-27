20a02394372aba1b037cc35c62cd7def6eb7525c
MariaShebeko <marygro89@gmail.com>
2022-01-27 09:32:42 +0200

    made redux for authorization

---
+++ src/redux/auth/auth-slice.js
@@ -0,0 +1,18 @@
+import { createSlice } from '@reduxjs/toolkit';
+import { authOperations } from './index';
+
+const initialState = {
+  user: { name: null, email: null },
+  token: null,
+  isLoggedIn: false,
+};
+
+const authSlice = createSlice({
+  name: 'auth',
+  initialState,
+  extraReducers: {
+    [authOperations.register.fulfilled](state, action) {},
+  },
+});
+
+export default authSlice.reducer;
