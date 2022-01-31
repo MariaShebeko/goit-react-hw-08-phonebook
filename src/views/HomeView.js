d32a6114843809af835c2a771f44f56a337cf928
MariaShebeko <marygro89@gmail.com>
2022-01-31 22:29:48 +0200

    added styles from mui on home page

--- src/views/HomeView.js
+++ src/views/HomeView.js
@@ -1,7 +1,25 @@
+import { Container, Typography, Stack } from '@mui/material';
+import PhoneInTalkIcon from '@mui/icons-material/PhoneInTalk';
+import image from '../images/home-image.jpg';
+import 'animate.css';
+
 const HomeView = () => {
   return (
     <>
-      <h1>HomeView</h1>
+      <Container maxWidth="lg">
+        <Stack direction="row">
+          <Typography variant="h1" component="h1" mt={20} align={'center'}>
+            YOUR PHONE BOOK
+          </Typography>
+          <img src={image} alt="homeImage" style={{ marginTop: '90px' }} />
+        </Stack>
+        <Stack direction="row" justifyContent={'center'}>
+          <Typography variant="h6" component="h1" mt={10} color={'#4f95da'}>
+            Put all your contacts in one place and you'll never forget them...{' '}
+            <PhoneInTalkIcon className={'animate__animated animate__bounce'} />
+          </Typography>
+        </Stack>
+      </Container>
     </>
   );
 };
