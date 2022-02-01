906356aaa3db6e2b2f2b08be0dead24cf923f3f0
MariaShebeko <marygro89@gmail.com>
2022-02-01 09:12:14 +0200

    added createSelector for filtered contacts

--- src/redux/contacts/contacts-selectors.js
+++ src/redux/contacts/contacts-selectors.js
@@ -1,11 +1,15 @@
+import { createSelector } from '@reduxjs/toolkit';
+
 export const getItems = state => state.contacts.items;
 export const getFilter = state => state.contacts.filter;
 export const getLoader = state => state.contacts.loading;
 
-export const getVisibleContacts = state => {
-  const { items, filter } = state.contacts;
-  const normalizedFilter = filter.toLowerCase();
-  return items.filter(contact =>
-    contact.name.toLowerCase().includes(normalizedFilter),
-  );
-};
+export const getVisibleContacts = createSelector(
+  [getItems, getFilter],
+  (items, filter) => {
+    const normalizedFilter = filter.toLowerCase();
+    return items.filter(contact =>
+      contact.name.toLowerCase().includes(normalizedFilter),
+    );
+  },
+);
